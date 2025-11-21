ce80be3e36f494674e3339d16225a286163a0286
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-11-21 14:39:10 +0100

    Added WIN32 specific classes

---
+++ src/lysa/os/win32/Renderer.cpp
@@ -0,0 +1,22 @@
+/*
+* Copyright (c) 2025-present Henri Michelon
+*
+* This software is released under the MIT License.
+* https://opensource.org/licenses/MIT
+*/
+module lysa.renderer;
+#include <windows.h>
+
+namespace lysa {
+
+    void Renderer::processPlatformEvents() const {
+        auto msg = MSG{};
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        EventManager::process();
+    }
+
+
+}
